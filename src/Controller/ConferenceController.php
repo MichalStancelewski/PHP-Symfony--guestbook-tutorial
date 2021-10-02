1cb8bf47e84ba0027aea76c95e1c0f9ea2817747
MichalStancelewski <michal@stancelewski.pl>
2021-10-02 13:18:19 +0200

    EP28/ added en/pl routing and language switcher

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -37,12 +37,18 @@ class ConferenceController extends AbstractController
         $this->bus = $bus;
     }
 
+    #[Route('/')]
+    public function indexNoLocale()
+    {
+        return $this->redirectToRoute('homepage', ['_locale' => 'en'], 301);
+    }
+
     /**
      * @throws SyntaxError
      * @throws RuntimeError
      * @throws LoaderError
      */
-    #[Route('/', name: 'homepage')]
+    #[Route('/{_locale<%app.supported_locales%>}/', name: 'homepage')]
     public function index(ConferenceRepository $conferenceRepository): Response
     {
         $response = new Response($this->twig->render('conference/index.html.twig', [
@@ -52,7 +58,7 @@ class ConferenceController extends AbstractController
         return $response;
     }
 
-    #[Route('/conference/header', name: 'conference_header')]
+    #[Route('/{_locale<%app.supported_locales%>}/conference/header', name: 'conference_header')]
     public function conferenceHeader(ConferenceRepository $conferenceRepository): Response
     {
         $response = new Response($this->twig->render('conference/header.html.twig', [
@@ -68,7 +74,7 @@ class ConferenceController extends AbstractController
      * @throws LoaderError
      * @throws Exception
      */
-    #[Route('/conference/{slug}', name: 'conference')]
+    #[Route('/{_locale<%app.supported_locales%>}/conference/{slug}', name: 'conference')]
     public function show(Request           $request, Conference $conference, CommentRepository $commentRepository,
                          NotifierInterface $notifier, string $photoDir): Response
     {
